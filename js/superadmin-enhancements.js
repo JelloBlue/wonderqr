a63468c5a48a938dd006b0450ad6c55543e3e954
JelloBlue <care.jelloblue@gmail.com>
2026-09-03 19:05:11 +0530

    Add Super Admin usage analytics module

--- js/superadmin-enhancements.js
+++ js/superadmin-enhancements.js
@@ -1,6 +1,7 @@
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from './config.js';
 import './superadmin-links.js';
 import './superadmin-link-buttons.js?v=2';
+import './superadmin-usage.js?v=1';
 
 const key = new URLSearchParams(location.search).get('key') || '';
 const api = `${SUPABASE_URL}/functions/v1/superadmin_api`;
