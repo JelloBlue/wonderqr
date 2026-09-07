52585ab30b2b12d66d053e10fd911e1c5de4ab7b
JelloBlue <care.jelloblue@gmail.com>
2026-09-08 00:24:51 +0530

    Phase 5: activate updated notification service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='wonderqr-admin-v2';
+const CACHE_NAME='wonderqr-admin-v3';
 const SHELL=['./admin.html','./manifest.json','./favicon.png','./js/pwa-install.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
